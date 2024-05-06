f428dc3efde4a7125e6565eff3d4d508c7bfe340
Hossam <hossam995@gmail.com>
2024-05-07 02:24:13 +0300

    Uploaded the project

---
+++ app/Filament/Widgets/CustomerCountOverview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Widgets;
+use App\Models\Customer;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class CustomerCountOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Customers', Customer::query()->count()),
+        ];
+    }
+}
