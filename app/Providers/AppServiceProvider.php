eaa025f032d7871cf3d1c889e0cd64771b3c45ed
Hossam <hossam995@gmail.com>
2024-05-24 22:13:10 +0300

    added arabic language for admin dashboard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
-
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,5 +23,15 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
         Schema::defaultStringLength(191);
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['ar','en','fr']) // also accepts a closure
+                ->visible(outsidePanels: true)
+                ->outsidePanelRoutes([
+                    'login',
+                    // Additional custom routes where the switcher should be visible outside panels
+                ]);
+        });
+
     }
 }
